0fba0c8acf25dea802ad54c832038f0fea1b907c
djcorea <javier.corea27@gmail.com>
2020-09-13 19:26:11 -0600

    Adding commts from console

---
+++ index.php
@@ -0,0 +1,10 @@
+<?PHP
+$names=array("Dany","Dylan","Lenci","Celio","Valentin");
+
+for ($i=0; $i <5 ; $i++) { 
+    # code...
+    echo "Storaged Name ".($i+1)." ".$names[$i];
+    echo "<br>";
+}
+ 
+?>
